f643e457a024e36c798f51c336a4b5f92c06341d
nRumbaII <sebastian.diesler@web.de>
2021-02-16 22:55:40 +0100

    edit overview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
+import 'package:banking/feature/overview/overview_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(Banking());
 }
 
+class Banking extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OverviewPage(),
+    );
+  }
+}
